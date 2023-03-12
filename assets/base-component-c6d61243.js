bb5eae1175f0275a128ea387582b3044d7981ec3
Aurora Chen <sy83228@gmail.com>
2023-03-12 23:00:05 +0800

    deploy

--- assets/base-component-c6d61243.js
+++ assets/base-component-c6d61243.js
@@ -1,4 +1,4 @@
-import{c as M}from"./index-174217fd.js";var q={},tt={get exports(){return q},set exports(h){q=h}};/*!
+import{a as M}from"./index-76caf2b8.js";var q={},tt={get exports(){return q},set exports(h){q=h}};/*!
   * Bootstrap index.js v5.2.3 (https://getbootstrap.com/)
   * Copyright 2011-2022 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
